f49a4ad7024f30b4bfa5f3bd90e330e24b6c8012
Mostafa Elsheikh <sasa94s@msn.com>
2018-07-23 03:14:37 +0200

    adding responsive flexbox grid layout

--- js/main.js
+++ js/main.js
@@ -155,7 +155,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
+  
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
