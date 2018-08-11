8ad845607f198447f655937adb5461c9a7181afc
Mostafa Elsheikh <sasa94s@msn.com>
2018-08-11 20:05:55 +0200

    feat: handling cache versioning and cleaning up

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
+const CACHE_STATIC_VERSION = 'v1';
+const CACHE_DYNAMIC_VERSION = 'v1';
+
 self.addEventListener('install', (event) => {
     console.log("[Service worker] Installing Service Worker...", event);
     event.waitUntil(
         // Open 'staatic' cache or create it if it doesn't exist
-        caches.open('static')
+        caches.open(`static_${CACHE_STATIC_VERSION}`)
             .then((cache) => {
                 console.log('[Service Worker] Precaching App Shell...');
                 // Static Precaching main assets
@@ -16,6 +19,22 @@ self.addEventListener('install', (event) => {
                 ]);
             })
     );
+});
+
+self.addEventListener('activate', (event) => {
+    console.log("[Service worker] Activating Service Worker...", event);
+    event.waitUntil(
+        caches.keys()
+            .then((keyList) => {
+                return Promise.all(keyList.map((key) => {
+                    if(key !== `static_${CACHE_STATIC_VERSION}` && key !== `dynamic_${CACHE_DYNAMIC_VERSION}`) {
+                        console.log('[Service Worker] Removing old cache...', key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
+    );
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
@@ -34,7 +53,7 @@ self.addEventListener('fetch', (event) => {
                         .then((serverResponse) => {
                             // Intercepts network request and returns it to original HTML when using `return` keyword
                             // Open 'dynamic' cache or create it if it doesn't exist
-                            return caches.open('dynamic')
+                            return caches.open(`dynamic_${CACHE_DYNAMIC_VERSION}`)
                                 .then((cache) => {
                                     // Storing a clone of network request in cache
                                     cache.put(event.request.url, serverResponse.clone());
