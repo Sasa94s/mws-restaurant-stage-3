6469670ec12730c8ebfcaf9b6f2f52f46027d942
Mostafa Elsheikh <sasa94s@msn.com>
2018-08-12 08:50:07 +0200

    feat: fallback cache

--- sw.js
+++ sw.js
@@ -1,17 +1,23 @@
 const CACHE_STATIC_VERSION = 'v1';
 const CACHE_DYNAMIC_VERSION = 'v1';
 
+const CACHE_STATIC_NAME = `static_${CACHE_STATIC_VERSION}`;
+const CACHE_DYNAMIC_NAME = `dynamic_${CACHE_DYNAMIC_VERSION}`;
+
 self.addEventListener('install', (event) => {
     console.log("[Service worker] Installing Service Worker...", event);
     event.waitUntil(
         // Open 'staatic' cache or create it if it doesn't exist
-        caches.open(`static_${CACHE_STATIC_VERSION}`)
+        caches.open(CACHE_STATIC_NAME)
             .then((cache) => {
                 console.log('[Service Worker] Precaching App Shell...');
                 // Static Precaching main assets
                 cache.addAll([
                     '/',
                     '/index.html',
+                    '/offline.html',
+                    '/img/sad.svg',
+                    '/img/refresh.svg',
                     '/js/dbhelper.js',
                     '/js/index.js',
                     '/js/main.js',
@@ -27,7 +33,7 @@ self.addEventListener('activate', (event) => {
         caches.keys()
             .then((keyList) => {
                 return Promise.all(keyList.map((key) => {
-                    if(key !== `static_${CACHE_STATIC_VERSION}` && key !== `dynamic_${CACHE_DYNAMIC_VERSION}`) {
+                    if(key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
                         console.log('[Service Worker] Removing old cache...', key);
                         return caches.delete(key);
                     }
@@ -53,11 +59,19 @@ self.addEventListener('fetch', (event) => {
                         .then((serverResponse) => {
                             // Intercepts network request and returns it to original HTML when using `return` keyword
                             // Open 'dynamic' cache or create it if it doesn't exist
-                            return caches.open(`dynamic_${CACHE_DYNAMIC_VERSION}`)
+                            return caches.open(CACHE_DYNAMIC_NAME)
                                 .then((cache) => {
                                     // Storing a clone of network request in cache
                                     cache.put(event.request.url, serverResponse.clone());
                                     return serverResponse;
+                                })
+                        })
+                        .catch((err) => {
+                            console.log("Can't connect!")
+                            // Returning offline fallback page
+                            return caches.open(CACHE_STATIC_NAME)
+                                .then((cache) => {
+                                    return cache.match('/offline.html');
                                 })
                         })
                 }
