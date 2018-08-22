ba1109d062531cc7b6edefad85cf56c431f935a5
Mostafa Elsheikh <sasa94s@msn.com>
2018-08-22 23:38:35 +0200

    feat:  handle sw registeration fail

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
-        .then(() => {
-            console.log("Service worker registered!");
+        .then((registeration) => {
+            console.log("[Index] Service worker registered!", registeration.scope);
+        })
+        .catch((error) => {
+            console.log("[Index] Service worker registeration failed", error);
         });
 }
